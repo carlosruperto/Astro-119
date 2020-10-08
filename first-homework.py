1bb63250705f07a932720a123f9f562aa44f25b8
carlosruperto <72216708+carlosruperto@users.noreply.github.com>
2020-10-08 07:20:02 -0700

    Add files via upload

---
+++ first-homework.py
@@ -0,0 +1,6 @@
+
+
+#this program is called first homework and will write my name and preferred pronouns
+#Carlos Ruperto (he/him/his)
+
+print("Carlos Ruperto (he/him/his") # print out Carlos Ruperto (he/him/his)
