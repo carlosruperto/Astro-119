1fe12fc2605f4c17d2cf0f821e04729f5f1f4621
carlosruperto <72216708+carlosruperto@users.noreply.github.com>
2020-10-08 07:22:36 -0700

    Delete first-homework.py

--- first-homework.py
+++
@@ -1,6 +0,0 @@
-
-
-#this program is called first homework and will write my name and preferred pronouns
-#Carlos Ruperto (he/him/his)
-
-print("Carlos Ruperto (he/him/his") # print out Carlos Ruperto (he/him/his)
